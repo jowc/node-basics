c77dc65161596b32993906b9daccb9a7259d8689
Joseph Chikeme <joecdev@gmail.com>
2023-02-19 21:22:30 +0100

    added express framework

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require("http")
+
+const express = require("express")
+
 const webRoutes = require('./routes/admin.routes')
 
-const server = http.createServer(webRoutes)
+const app = express()
+
+app.use((req, res, next) => {
+    console.log("Added nodeJs middleware")
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log("another nodeJs middleware")
+})
+
+const server = http.createServer(app)
 
 server.listen(3000)
 console.log('server running on: http://localhost:3000')
